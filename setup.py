5c53fa3833e2e7ff2f292606d8c4ed06ba4f68f7
andylizf <andylizf@outlook.com>
2024-05-24 16:36:46 +0800

    chore: different build modes in setup.py

--- setup.py
+++ setup.py
@@ -7,11 +7,14 @@ from torch.utils.cpp_extension import CUDA_HOME, BuildExtension, CUDAExtension
 
 LIBRARY_NAME = "trec"
 EXTENSION_NAME = f"{LIBRARY_NAME}._C"
-EXTENSION_DIR = os.path.join(os.path.dirname(
-    os.path.curdir), LIBRARY_NAME, "csrc")
-DEBUG_MODE = os.getenv("DEBUG", "0") == "1"
+EXTENSION_DIR = os.path.join(os.path.dirname(os.path.curdir), LIBRARY_NAME, "csrc")
 INCLUDE_DIRS = [EXTENSION_DIR]
 DEFINE_MACROS = [("WITH_CUDA", None)]
+
+DEBUG_MODE = os.getenv("DEBUG", "0") == "1"
+PROFILE_MODE = os.getenv("PROFILE", "0") == "1"
+RELEASE_MODE = os.getenv("RELEASE", "0") == "1"
+PRINT_FLAGS = os.getenv("PRINT", "0") == "1"
 
 
 class MyBuildExtension(BuildExtension):
@@ -24,10 +27,10 @@ class MyBuildExtension(BuildExtension):
         for ext in self.extensions:
             compile_args = ext.extra_compile_args
             for include_dir in ext.include_dirs:
-                if 'cxx' in compile_args:
-                    compile_args['cxx'] += ['-isystem', f'{include_dir}']
-                if 'nvcc' in ext.extra_compile_args:
-                    compile_args['nvcc'] += ['-isystem', f'{include_dir}']
+                if "cxx" in compile_args:
+                    compile_args["cxx"] += ["-isystem", f"{include_dir}"]
+                if "nvcc" in ext.extra_compile_args:
+                    compile_args["nvcc"] += ["-isystem", f"{include_dir}"]
         super().build_extensions()
 
 
@@ -39,11 +42,9 @@ def get_extensions():
 
     extra_compile_args = {
         "cxx": [
-            "-O3",
             "-std=c++20",
         ],
         "nvcc": [
-            "-O3",
             "-std=c++20",
             # CUDA specific
             "-DCUDA_HAS_FP16=1",  # ? not sure if this is needed
@@ -58,14 +59,24 @@ def get_extensions():
     }
     extra_link_args = []
 
-    if DEBUG_MODE:
-        pass
-    else:
+    if RELEASE_MODE:
+        extra_compile_args["cxx"] += ["-O3"]
+        extra_compile_args["nvcc"] += ["-O3"]
+    elif DEBUG_MODE:
+        extra_compile_args["cxx"] += ["-g", "-Og"]
+        extra_compile_args["nvcc"] += ["-g", "-G"]  # suppress device opt
+        extra_link_args += ["-g"]
+    else:  # default to PROFILE_MODE
+        extra_compile_args["cxx"] += ["-O3", "-g", "-pg", "-lineinfo"]
+        extra_compile_args["nvcc"] += ["-O3", "-g", "-pg"]
+
+    if not PRINT_FLAGS:
         extra_compile_args["cxx"] += ["-DNDEBUG"]
         extra_compile_args["nvcc"] += ["-DNDEBUG"]
 
-    sources = glob.glob(os.path.join(EXTENSION_DIR, "**", "*.cpp"), recursive=True) + \
-        glob.glob(os.path.join(EXTENSION_DIR, "**", "*.cu"), recursive=True)
+    sources = glob.glob(
+        os.path.join(EXTENSION_DIR, "**", "*.cpp"), recursive=True
+    ) + glob.glob(os.path.join(EXTENSION_DIR, "**", "*.cu"), recursive=True)
 
     CC = os.environ.get("CC", None)
     if CC is not None:

a1152e13e4740ae06fe06441467a957cee517c12
Andy Lee <andylizf@outlook.com>
2024-11-01 07:11:08 +0000

    feat: add resnet

--- examples/scripts/train_resnet_on_cifar10_template.sh
+++ examples/scripts/train_resnet_on_cifar10_template.sh
@@ -6,22 +6,20 @@ DATASET_DIR=data
 python train_model.py \
 --checkpoint_path=${CHECKPOINT_PATH} \
 --dataset_path=${DATASET_DIR} \
---model_name=densenet \
+--model_name=resnet \
 --epochs=100 \
---batch_size=64 \
+--batch_size=128 \
 --learning_rate=0.1 \
 --momentum=0.9 \
 --weight_decay=1e-4 \
---depth=40 \
---k=12 \
---trec=0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 \
+--trec=1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1 \
 --L=9,\
-3,9,3,9,3,9,3,9,3,9,3,9,\
+3,9,3,9,3,9,\
 6,\
-6,9,6,9,6,9,6,9,6,9,6,9,\
+3,9,3,9,3,9,\
 6,\
-6,9,6,9,6,9,6,9,6,9,6,9 \
---H=8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8 \
+3,9,3,9,3,9 \
+--H=8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8 \
 --gpu=0
 
 echo "==============================="
@@ -30,13 +28,11 @@ echo "==============================="
 python train_model.py \
 --checkpoint_path=${CHECKPOINT_PATH}_baseline \
 --dataset_path=${DATASET_DIR} \
---model_name=densenet \
+--model_name=resnet \
 --epochs=100 \
---batch_size=64 \
+--batch_size=128 \
 --learning_rate=0.1 \
 --momentum=0.9 \
 --weight_decay=1e-4 \
---depth=40 \
---k=12 \
---trec=0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 \
+--trec=0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 \
 --gpu=0
